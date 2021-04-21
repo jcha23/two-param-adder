8c6b57db2536b02bf4c14e421eef1d1cbd52e0dd
GSM1900\JChang23 <john.chang23@t-mobile.com>
2021-04-20 22:35:57 -0700

    updated properites

--- src/main/java/com/pttbc/twoparamadder/TwoParamAdderApplication.java
+++ src/main/java/com/pttbc/twoparamadder/TwoParamAdderApplication.java
@@ -2,12 +2,23 @@ package com.pttbc.twoparamadder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import dto.TwoParamAdderResponse;
+
+@Controller
 @SpringBootApplication
 public class TwoParamAdderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TwoParamAdderApplication.class, args);
 	}
-
+	
+	@RequestMapping("/{num1}/{num2}")
+	public TwoParamAdderResponse getTwoParamAdder(@PathVariable Long num1, @PathVariable Long num2) {
+		return new TwoParamAdderResponse(num1, num2);
+	}
 }
