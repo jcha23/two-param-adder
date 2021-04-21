1417677ee887392525de5347dc709ef38937b1c4
GSM1900\JChang23 <john.chang23@t-mobile.com>
2021-04-20 19:00:06 -0700

    Initial commit

---
+++ src/main/java/com/pttbc/twoparamadder/TwoParamAdderApplication.java
@@ -0,0 +1,13 @@
+package com.pttbc.twoparamadder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoParamAdderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoParamAdderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pttbc/twoparamadder/TwoParamAdderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pttbc.twoparamadder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoParamAdderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
