d8bbdcc2849dafc7f18a43084a88c73dda027d17
GSM1900\JChang23 <john.chang23@t-mobile.com>
2021-04-20 22:55:48 -0700

    missing dependencies

--- src/main/java/com/pttbc/twoparamadder/TwoParamAdderApplication.java
+++ src/main/java/com/pttbc/twoparamadder/TwoParamAdderApplication.java
@@ -9,16 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import dto.TwoParamAdderResponse;
 
-@Controller
 @SpringBootApplication
 public class TwoParamAdderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TwoParamAdderApplication.class, args);
 	}
-	
-	@RequestMapping("/{num1}/{num2}")
-	public TwoParamAdderResponse getTwoParamAdder(@PathVariable Long num1, @PathVariable Long num2) {
-		return new TwoParamAdderResponse(num1, num2);
-	}
+
 }
--- src/main/java/controller/TwoParamAdderController.java
+++ src/main/java/controller/TwoParamAdderController.java
@@ -1,8 +1,20 @@
 package controller;
 
 import dto.TwoParamAdderResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping(value = "/backend")
 public class TwoParamAdderController {
 	
+	@RequestMapping(value = "/sum/{num1}/{num2}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> getTwoParamAdder(@PathVariable Long num1, @PathVariable Long num2) {
+		TwoParamAdderResponse twoParamAdderResponse = new TwoParamAdderResponse(num1, num2);
+		String responseJson = "{\"sum\": \"" + twoParamAdderResponse.getSum() + "\"}";
+		return new ResponseEntity<>(responseJson, HttpStatus.OK);
+	}
 }
